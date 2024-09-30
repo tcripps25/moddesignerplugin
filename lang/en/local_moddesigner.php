ce25e515fd9e12487cc9e6fbe9bf3a0440970ac7
Tom Cripps <tcripps25@gmail.com>
2024-09-30 17:52:17 +0100

    strings

--- lang/en/local_moddesigner.php
+++ lang/en/local_moddesigner.php
@@ -1,4 +1,44 @@
 <?php
-
+// Global Strings
 $string['pluginname'] = 'Module Designer';
 $string['appname'] = 'Module Designer';
+$string['appsubtitle'] = 'Plugin UI3';
+
+// Sidebar Menu
+// Section 1
+$string['section1'] = 'Getting started';
+// Page titles
+$string['staticmenu1'] = 'What is Module Designer?';
+$string['staticmenu2'] = 'How to begin';
+// Main Menu
+$string['mainmenu1'] = 'Set Up';
+$string['mainmenu2'] = 'Design';
+$string['mainmenu3'] = 'Visualise';
+$string['mainmenu4'] = 'Publish';
+// Design Menu
+$string['designmenutitle1'] = 'Module Schedule';
+$string['addweekbtn'] = 'Add Week';
+
+// Views
+// Schedule View
+$string['schedulepaneltitle'] = 'Teaching Weeks';
+$string['gotoweekbtn'] = 'Go to week';
+$string['addmultiweeksbtn'] = 'Add multiple weeks';
+
+// Activity Edit/Creation Dialog
+$string['steppaneltitle1'] = 'Basic Information';
+$string['form_activity_titlelabel'] = 'Title';
+$string['form_activity_titlehelp'] = 'Enter a descriptive title for this Activity.';
+$string['form_activity_instructionslabel'] = 'Instructions';
+$string['form_activity_instructionshelp'] = 'Describe the steps involved in this Activity to your students.';
+
+$string['form_activity_duration_label'] = 'Duration (mins)';
+$string['form_activity_duration_help'] = 'How long this Activity will take to complete.';
+$string['form_activity_group_label'] = 'Group';
+$string['form_activity_group_help'] = 'Is this a group Activity?';
+$string['form_activity_type_label'] = 'Learning Type';
+$string['form_activity_type_help'] = 'Select one or more Learning Types for this Activity.';
+
+$string['steppaneltitle2'] = 'Learning Approach';
+$string['steppaneltitle3'] = 'Moodle Activities';
+$string['steppaneltitle4'] = 'Alignments';
