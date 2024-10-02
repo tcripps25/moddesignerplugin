8237d02ecee6e52f4dae4ab4b40047ce2a575a14
Tom Cripps <tcripps25@gmail.com>
2024-10-02 16:01:40 +0100

    update

--- ajax.php
+++ ajax.php
@@ -3,6 +3,10 @@ require_once('../../config.php');
 use local_moddesigner\output\designerdata;
 
 require_login(); // Ensure the user is logged in
-require_sesskey();
+require_sesskey(); // session key
+
+// Rich suggested adding something else here for security but I've forgotton - Tom
+
+
 
 echo json_encode(['modules' => DesignerData::moddesigner_get_all_modules()]);
