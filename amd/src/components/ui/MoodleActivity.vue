932b3da8928d7d7ddd0cbefc0d0adad1ac6f139f
Tom Cripps <tcripps25@gmail.com>
2024-10-04 11:38:05 +0100

    update to moodle activities

--- amd/src/components/ui/MoodleActivity.vue
+++ amd/src/components/ui/MoodleActivity.vue
@@ -1,9 +1,7 @@
 <script setup>
-import Button from 'primevue/button';
-import { ref, watch, computed } from 'vue';
-import { CheckCircleIcon } from '@heroicons/vue/24/solid';
+
 import Pbutton from '../buttons/Pbutton.vue';
-import { PlusCircleIcon, TrashIcon } from '@heroicons/vue/16/solid';
+import { PlusCircleIcon, TrashIcon, ArrowUpIcon, ArrowDownIcon } from '@heroicons/vue/16/solid';
 
 const props = defineProps({
   moodleActivity: Object,
@@ -14,7 +12,19 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="flex gap-1">
+  <div class="flex items-center gap-1">
+    <div v-if="selected">
+      <Pbutton aria-label="Move item up">
+        <template #icon>
+          <ArrowUpIcon class="h-4 w-4" />
+        </template>
+      </Pbutton>
+      <Pbutton aria-label="Move item down">
+        <template #icon>
+          <ArrowDownIcon class="h-4 w-4" />
+        </template>
+      </Pbutton>
+    </div>
     <Pbutton :disabled="selected" :class="selected ? '!w-full cursor-move hover:shadow-sm hover:ring-2' : ''" trailing
       ghost :label="moodleActivity.type ? moodleActivity.name + ' (' + moodleActivity.type + ')' : moodleActivity.name"
       @click="$emit('activityClicked')">
